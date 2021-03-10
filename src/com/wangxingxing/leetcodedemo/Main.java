8492caa2f6d4b6327afde7458d46eea2131dbe9a
WangXingxing <xingfengwxx@gmail.com>
2021-03-10 17:47:26 +0800

    Initial commit

---
+++ src/com/wangxingxing/leetcodedemo/Main.java
@@ -0,0 +1,9 @@
+package com.wangxingxing.leetcodedemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world!");
+    }
+}
